e292c51ab96ea5bfabb22b68e98e33a70bfb64b3
Jagdeep Tiwana <jtiwana@hotmail.com>
2024-06-04 13:25:15 +0100

    Added Header code and style

--- src/App.js
+++ src/App.js
@@ -4,9 +4,34 @@ function App() {
   return (
     <>
       <header>
-        <h1 className="text-red-900">
-          Jag's portfolio ..... Nav, Projects, About me
-        </h1>
+        <div className="container m-auto px-4 py-6">
+          <div className="flex justify-between items-center">
+            <div>
+              <h1 className="font-bold text-2xl text-red-600 gradiant-text running-text ">
+                Jag's Portfolio
+              </h1>
+            </div>
+            <div>
+              <ul className="flex gap-4 text-xl">
+                <li>
+                  <a className="text-gray-400 hover:text-red-600 cursor-pointer">
+                    Projects
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-400 hover:text-red-600 cursor-pointer">
+                    Technologies
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-400 hover:text-red-600 cursor-pointer">
+                    About me
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </header>
       <main>
         {/* INTRO BANNER SECTION */}
